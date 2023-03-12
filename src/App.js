6840d7d50dd379abffa720709785ee28671814b0
Masih <masihdon@hotmail.de>
2023-03-12 15:19:00 +0100

    Makeover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styles from './styles/App.css';
+import Header from "./Header"
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [randomProductId, setRandomProductId] = useState(null);
+  const [showAllProducts, setShowAllProducts] = useState(false);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+      });
+  }, []);
+
+  const randomize = () => {
+    const randomProductId = Math.floor(Math.random() * products.length) + 1;
+    setRandomProductId(randomProductId);
+    setShowAllProducts(false);
+  };
+
+  const filteredProducts = randomProductId
+    ? products.filter((product) => product.id === randomProductId)
+    : products;
+
+    function ratingToStars(rating) {
+  let stars = '';
+  
+  for (let i = 1; i <= 5; i++) {
+    if (i <= rating) {
+      stars += '★';
+    } else {
+      stars += '☆';
+    }
+  }
+  
+  return stars;
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <div className="app">
+     
+          <button className='random-btn'
+            onClick={randomize}
+          >
+            Get a random Product
+          </button>
+
+          <div className='container'>
+           
+            {filteredProducts.map((product) => (
+              <div key={product.id}>
+                <div className="card">
+                  <div>
+                    <div className='left'>
+                      <h1 className='title'>{product.title}</h1>
+                      <p >
+                        {product.description}
+                      </p>
+                      <p>Special-Price: {product.price}</p>
+                      <button className='button'>Add to Cart</button>
+                      
+                    </div>
+                    <div className='right'>
+                      <img className='image' src={product.thumbnail} alt={product.title} />
+                      <p className='rating'>{ratingToStars(product.rating)}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+    
+    
+    </>
   );
 }
 
